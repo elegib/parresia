5f3b109b8b55cd45019fb161ec83ce5721aa9b87
elegib <43402323+elegib@users.noreply.github.com>
2021-02-07 01:10:31 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+function sali() {
+  $("#intro").addClass("su");
+  $("#link").addClass("su");
+  $("#presentazione").addClass("su")
+}
+setTimeout(sali, 3500);
+  //$("#projects a").mouseover(function() {$(this).addClass("underline")}).mouseout(function(){$(this).removeClass("underline")})
+
+
+})
